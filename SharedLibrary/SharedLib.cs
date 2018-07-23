eeaa402db8ca621a66dbb3a7a66e7ae8eed12e2c
Kevin Junghans <kevin.junghans@emerson.com>
2018-07-23 14:45:38 -0400

    some mods

--- SharedLibrary/SharedLib.cs
+++ SharedLibrary/SharedLib.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace SharedLibrary
 {
     public class SharedLib
     {
-        private const string version = "1.0.1";
 
         public string GetVersion()
         {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
             return version;
         }
     }
