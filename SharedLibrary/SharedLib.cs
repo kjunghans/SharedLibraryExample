870c85f147f4f3285cd7f54fdd69d39d606fbf73
Kevin Junghans <kevin.junghans@acm.org>
2018-09-07 12:55:32 -0400

    Added shared library and its own solution

--- SharedLibrary/SharedLib.cs
+++ SharedLibrary/SharedLib.cs
@@ -1,17 +1,29 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
-namespace SharedLibrary
+namespace MyLibrary
 {
+    /// <summary>
+    /// This is a shared library that returns the assemblies version
+    /// </summary>
     public class SharedLib
     {
-
-        public string GetVersion()
+        public string GetFileVersion()
         {
             System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
             FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
             string version = fvi.FileVersion;
             return version;
         }
+
+        public string GetProductVersion()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.ProductVersion;
+            return version;
+        }
+
+
     }
 }
+
